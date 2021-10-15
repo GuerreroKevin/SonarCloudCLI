c51890ff0b2ac3e3c7a71367ec50a95ec280f0f0
GuerreroKevin <kevin.gro.99@gmail.com>
2021-10-14 20:10:03 -0700

    first commit

---
+++ PruebaGestion/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PruebaGestion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Mundo.\n Esto es una prueba para la clase de Gestion de la Calidad del Software II.");
+        }
+    }
+}
